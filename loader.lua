941cbe399fd7bb6fae3e455c27435c7ae1dfcbab
MxdLL <144973018+MxdLL@users.noreply.github.com>
2026-09-18 02:31:49 +0700

    Update loader.lua [2KHUB real-time sync]

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MxdLL/2khub/main/2ski_ride_a_pet_protected.lua?t=" .. tick()))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MxdLL/2khub/main/2ski_ride_a_pet_v3_protected.lua?t=" .. tick()))()
